62d0217d3df2b261b8bb33795baa65f1fccd19cb
waedkhalifa <62521979+waedkhalifa@users.noreply.github.com>
2021-11-11 15:14:38 +0200

    initial catalog

--- main.py
+++ main.py
@@ -1,28 +1,40 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 
 app = Flask(__name__)
-
-f = open('BooksFourEntries.json', )
-data = json.load(f)
 
 
 @app.route('/search/<string:topic>', methods=['GET'])
 def searchByBookTopic(topic):
     displayed = []
+
+    f = open('FourEntries.json', )
+    data = json.load(f)
+
     for i in data['books']:
         if i['topic'] == topic:
             displayed.append({'id': i['id'], 'title': i['title']})
+
+    f.close()
+
     return jsonify(displayed)
 
-@app.route('/info/<id>', methods=['GET'])
+
+@app.route('/info/<int:id>', methods=['GET'])
 def searchByBookId(id):
+    displayed = []
+
+    f = open('FourEntries.json', )
+    data = json.load(f)
+
     for i in data['books']:
         if int(id) == i['id']:
-            return jsonify(i)
-    f.close()
+            displayed.append({'title': i['title'], 'quantity': i['quantity'], 'price': i['price']})
+            f.close()
+            return jsonify(displayed)
+
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=7000)
