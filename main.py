5a8b3148bb2ea2c08f2988abc77a55b687273ad3
diala-eng <71458903+diala-eng@users.noreply.github.com>
2021-12-23 21:14:58 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 import json
-
+import requests
 app = Flask(__name__)
 
 
@@ -13,12 +13,13 @@ def searchByBookTopic(topic):
 
     for i in data['books']:
         if i['topic'] == topic:
-            displayed.append({'id': i['id'], 'title': i['title']})
+            displayed.append({'id': i['id'],'price':i['price'],'quantity': i['quantity'],'title': i['title'],'tpoic':i['topic']})
     f.close()
 
     if len(displayed) == 0:
         return '404 ERROR, NOT FOUND', 404
     else:
+        print('dddddoneeee')
         return jsonify(displayed)
 
 
@@ -45,7 +46,7 @@ def updateQuantity(id):
     data = json.load(f)
 
     key_to_update = update_req['quantity']  # quantity
-    print(update_req['quantity'])
+
 
     for i in data['books']:
         if int(id) == i['id']:
@@ -54,9 +55,13 @@ def updateQuantity(id):
                 return 'NOT ACCEPTABLE', 406
 
             else:
+                m=i['quantity']
+                print(m)
                 i['quantity'] = key_to_update
-                displayed.append({'id': i['id'], 'price': i['price'], 'quantity': i['quantity'], 'title': i['title'],
-                                  'topic': i['topic']})
+                displayed.append({'id': i['id'], 'price': i['price'], 'quantity': i['quantity'], 'title': i['title'],'topic': i['topic']})
+
+                #requests.put('http://192.168.56.104:7777/updateinfo/{}'.formate(id),json=(displayed))
+                request.post('http://192.168.56.102:6000/invalidate', json=(displayed))
     f.close()
 
     FEfile = open("FourEntries.json", "w")
